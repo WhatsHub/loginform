250d16735f7f78b3bab43cdca2bd70af11b73556
Joschua Fink <joschua.fink@gmx.de>
2019-06-19 19:44:04 +0200

    added table of registered users on intern page.

--- intern.php
+++ intern.php
@@ -20,5 +20,28 @@ if (!isset($_SESSION["userid"])){
 Hello <?php echo $_SESSION["userid"] ?> Congratulations you logged in succesfully and can now see this secret website!
 <br>
 <br>
+
+Here is the table of all users that are currently registered: <br> <br>
+
+<table>
+   <tr> <th>id</th>  <th>email</th> <th>password</th> <th>firstname</th> <th>lastname</th> <th>created at</th> <th>updated at</th> </tr>
+
+    <?php
+
+        // get all the entrys from the table
+        $result = $conn->query("SELECT * FROM users");
+       if ($result->num_rows > 0){
+           while($row = $result->fetch_assoc()){
+               echo "<tr><td>" . $row['id'] . "</td><td>" .$row['email'] . "</td><td>".$row['password']."</td><td>" .
+                   $row['firstname'] . "</td><td>".$row['lastname']." </td><td>".$row['created_at']."</td><td>".$row['updated_at']."</td></tr>";
+
+           }
+       }
+       else{
+           echo "<tr><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td></tr>";
+       }
+    ?>
+</table>
+
 <a href="logout.php">Logout.</a>
 </body>
